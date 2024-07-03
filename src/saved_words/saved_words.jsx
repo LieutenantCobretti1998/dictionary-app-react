c3e2bcbfac8e4be6f82f49bfc4100b29f4f885fe
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-07-03 19:10:14 +0400

    work on logic in saved words

--- src/saved_words/saved_words.jsx
+++ src/saved_words/saved_words.jsx
@@ -1,14 +1,36 @@
 import "./saved_words.css";
 export default function SavedWords({savedWords, onSearch}) {
     const words = savedWords();
+    if(words.length > 0) {
+        return (
+            <div className="saved-words">
+            <h2 className="saved-words__title">Saved Words</h2>
+            <ul className="list">
+                {words.map((word, i) => (
+                    <li onClick={() => onSearch(word)} key={i}>
+                        <span>{word}</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="ionicon" viewBox="0 0 512 512">
+                            <path d="M112 112l20 320c.95 18.49 14.4 32 32 32h184c17.67 0 30.87-13.51 32-32l20-320"
+                                  fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                  strokeWidth="32"/>
+                            <path stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="32"
+                                  d="M80 112h352"/>
+                            <path
+                                d="M192 112V72h0a23.93 23.93 0 0124-24h80a23.93 23.93 0 0124 24h0v40M256 176v224M184 176l8 224M328 176l-8 224"
+                                fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                strokeWidth="32"/>
+                        </svg>
+                    </li>
+                ))}
+            </ul>
+            </div>
+        )
+    }
+
     return (
         <div className="saved-words">
-        <h2 className="saved-words__title">Saved Words</h2>
-        <ul className="list">
-            {words.map((word, i) => (
-                <li onClick={() => onSearch(word)} key={i}>{word}</li>
-            ))}
-        </ul>
+            <h2 className="saved-words__title">There are no save words😣</h2>
         </div>
     )
+
 }
